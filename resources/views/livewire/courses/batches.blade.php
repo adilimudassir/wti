be4450c743b4c682c1841bbde1895f47633af66d
Mudassir Ahmad Adili <mudassiradili4u@gmail.com>
2022-01-19 08:09:44 +0100

    wip

--- resources/views/livewire/courses/batches.blade.php
+++ resources/views/livewire/courses/batches.blade.php
@@ -43,7 +43,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($course->batches()->orderBy('id', 'DESC')->get() as $batchModel)
+                        @foreach($course->batches()->orderBy('created_at', 'DESC')->get() as $batchModel)
                         <tr class="@if($confirming === $batchModel->id) bg-primary fs-1 text-white @else fw-bold fs-6 text-gray-800 border-bottom border-gray-200 @endif">
                             <td class="fw-bold">
                                 {{ $batchModel->name }} <span class="badge badge-{{ $batchModel->active ? 'success' : 'info' }}">
