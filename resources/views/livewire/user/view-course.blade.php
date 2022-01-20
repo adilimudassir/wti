eb24eabf4fbe74e0e29677cfd2eb0e1615781c3a
Mudassir Ahmad Adili <mudassiradili4u@gmail.com>
2022-01-20 13:17:53 +0100

    wip

--- resources/views/livewire/user/view-course.blade.php
+++ resources/views/livewire/user/view-course.blade.php
@@ -39,38 +39,40 @@
         </x-slot>
         <x-slot name="toolbar">
             @if($userCourse->canMakePayment())
-            <x-button name="create" :href="route('payments.create', ['user_course_id' => $userCourse->id])" permission="create-payments" class="btn btn-sm btn-primary" icon="bi bi-plus" />
+            <x-button name="make payment" :href="route('payments.create', ['user_course_id' => $userCourse->id])" permission="create-payments" class="btn btn-sm btn-primary" icon="bi bi-plus" />
             @endif
         </x-slot>
         <x-slot name="body">
-            <table class="table table-rounded table-striped border gy-7 gs-7">
-                <thead>
-                    <tr class="fw-bold fs-6 text-gray-800 border-bottom border-gray-200">
-                        <th>Amount Paid</th>
-                        <th>Amount Due</th>
-                        <th>Status</th>
-                        <th>Type</th>
-                        <th>Method</th>
-                        <th>Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($userCourse->payments as $payment)
-                    <tr>
-                        <td>{{ currency($payment->amount) }}</td>
-                        <td>{{ currency($payment->amountDue()) }}</td>
-                        <td>
-                            <span class="badge badge-primary">
-                                {{ $payment->status() }}
-                            </span>
-                        </td>
-                        <td>{{ $payment->type }}</td>
-                        <td>{{ $payment->method }}</td>
-                        <td>{{ $payment->created_at->diffForHumans() }}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table table-rounded table-striped border gy-7 gs-7">
+                    <thead>
+                        <tr class="fw-bold fs-6 text-gray-800 border-bottom border-gray-200">
+                            <th>Amount Paid</th>
+                            <th>Amount Due</th>
+                            <th>Status</th>
+                            <th>Type</th>
+                            <th>Method</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($userCourse->payments as $payment)
+                        <tr>
+                            <td>{{ currency($payment->amount) }}</td>
+                            <td>{{ currency($payment->amountDue()) }}</td>
+                            <td>
+                                <span class="badge badge-primary">
+                                    {{ $payment->status() }}
+                                </span>
+                            </td>
+                            <td>{{ $payment->type }}</td>
+                            <td>{{ $payment->method }}</td>
+                            <td>{{ $payment->created_at->diffForHumans() }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </x-slot>
     </x-card>
 </div>
