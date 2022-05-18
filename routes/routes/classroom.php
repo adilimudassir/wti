45a7c15f00ddefc24e0d6c52df586c16561efe1b
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-05-18 14:43:24 +0100

    remove protection from impersonation

--- routes/routes/classroom.php
+++ routes/routes/classroom.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ClassroomController;
 
-Route::group(['prefix' => 'classroom', 'as' => 'classroom.', 'middleware' => 'impersonate.protect'], function () {
+Route::group(['prefix' => 'classroom', 'as' => 'classroom.'], function () {
     Route::get('', [ClassroomController::class, 'index'])->name('index');
     Route::get('/{course}/{level}/{topic}', [ClassroomController::class, 'show'])->name('show');
 });
