682248ba5bad0508f5854686bf0c0d6c9ff194f0
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-07-17 09:34:27 +0100

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,8 @@ import './bootstrap';
 // import './elements/turbo-echo-stream-tag';
 // import './libs/turbo';
 
-import Alpine from 'alpinejs';
+// import Alpine from 'alpinejs';
 
-window.Alpine = Alpine;
+// window.Alpine = Alpine;
 
-Alpine.start();
+// Alpine.start();
