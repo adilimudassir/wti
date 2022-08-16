b1f527cb8df9d5fbc4315a355c01d446b2988726
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-08-16 09:04:29 +0100

    update dropdown btn

--- resources/views/students/actions.blade.php
+++ resources/views/students/actions.blade.php
@@ -1,61 +1,29 @@
-<!--begin::Trigger-->
-<button type="button" class="btn btn-sm btn-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start">
-    Actions
-    <i class="bi bi-chevron-down"></i>
-</button>
-<!--end::Trigger-->
+<div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+    <button id="btnGroupDrop1" type="button" class="btn btn-light-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        Actions
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+        <li>
+            <x-button name="view" :href="route('students.show', $model)" permission="read-students" class="dropdown-item" />
+        </li>
+        <li>
+            <x-button name="edit" :href="route('students.edit', $model)" permission="update-students" class="dropdown-item" />
 
-<!--begin::Menu-->
-<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-250px py-4" data-kt-menu="true">
-    <!--begin::Menu item-->
-    <div class="menu-item px-3">
-        <x-button 
-            name="view" 
-            :href="route('students.show', $model)" 
-            permission="read-students" 
-            class="menu-link px-3" />
-    </div>
-    <!--end::Menu item-->
-
-    <!--begin::Menu item-->
-    <div class="menu-item px-3">
-        <x-button 
-            name="edit" 
-            :href="route('students.edit', $model)" 
-            permission="update-students" 
-            class="menu-link px-3"
-        />
-    </div>
-    <!--end::Menu item-->
-
-    @if ($model->isActive())
-    <!--begin::Menu item-->
-    <div class="menu-item px-3">
+        </li>
+        @if ($model->isActive())
         @canBeImpersonated($model)
-        <x-button.link 
-            :href="route('impersonate', $model->id)" 
-            class="menu-link px-3" 
-            :text="'Login as ' . $model->name" 
-            permission="impersonate-students" 
-        />
+        <li>
+            <x-button.link :href="route('impersonate', $model->id)" class="dropdown-item" :text="'Login as ' . $model->name" permission="impersonate-students" />
+        </li>
         @endCanBeImpersonated
-        <x-button.link 
-            :href="route('students.change-password', $model->id)" 
-            class="menu-link px-3" 
-            :text="'Change Password'" 
-            permission="update-students"
-        />
-    </div>
-    @endif
-    <!--end::Menu item-->
-    <div class="px-3">
-        @if ($model->id !== 1 && $model->id !== auth()->id())
-        <x-button.delete 
-            :href="route('students.delete', $model)" 
-            permission="delete-students" 
-            class="btn btn-light-primary w-100 px-3 btn-sm"
-        >Delete</x-button.delete>
+        <li>
+            <x-button.link :href="route('students.change-password', $model->id)" class="dropdown-item" :text="'Change Password'" permission="update-students" />
+        </li>
         @endif
-    </div>
+        <li>
+            @if ($model->id !== 1 && $model->id !== auth()->id())
+            <x-button.delete :href="route('students.delete', $model)" permission="delete-students" class="btn btn-light-primary w-100 px-3 btn-sm">Delete</x-button.delete>
+            @endif
+        </li>
+    </ul>
 </div>
-<!--end::Menu-->
